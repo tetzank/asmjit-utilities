1ac27bd3ff72c0527dcd6f5d716dc7f7812e7a0c
tetzank <tetzank@users.sourceforge.net>
2019-11-08 17:58:55 +0100

    added default value to PerfCompiler ctor to make it compatible with Compiler

--- perf/perfcompiler.h
+++ perf/perfcompiler.h
@@ -20,14 +20,18 @@ private:
 	JitDump jd;
 
 public:
-	explicit PerfCompiler(asmjit::CodeHolder *code) noexcept;
+	explicit PerfCompiler(asmjit::CodeHolder *code=nullptr) noexcept;
 	virtual ~PerfCompiler();
 
 	// implicitly attached to latest node
+	// adds file and line number as debug info to last created node (Compiler::cursor())
 	void attachDebugLine(const char *file_name=__builtin_FILE(), int line_number=__builtin_LINE());
-	//
+
+	// add code of function to dump file
 	void addCodeSegment(const char *fn_name, void *fn, uint64_t code_size);
-	//
+
+	// serialize sequence of nodes to machine code with Assembler
+	// additionally get offset of instruction and store it to debug information
 	asmjit::Error finalize();
 };
 
